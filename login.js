1bb6a8535369ec5310b80074d7ff58cca955cfb8
GOKULMATHI <108513958+GOKULMATHI@users.noreply.github.com>
2023-04-05 11:21:46 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './login.css';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle login logic here
+  };
+
+  return (
+    <div className="login-container" id="login">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit"><a href="#Home" style={{ textDecoration: 'none',color:"white" }}>Login</a></button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
